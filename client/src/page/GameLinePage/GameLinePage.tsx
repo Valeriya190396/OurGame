7df412348b0123d3a39ab329c58eceee51428024
Valeriya190396 <valerinakom@mail.ru>
2024-07-05 15:51:26 +0300

    draft_w/t_DB

--- client/src/page/GameLinePage/GameLinePage.tsx
+++ client/src/page/GameLinePage/GameLinePage.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { GameLine } from "../../entities/gameLines/types/gameLineTypes";
 
-import './GameLinePage.css'
+import "./GameLinePage.css";
 
 import ModalWindow from "../../shared/ui/Modal/Modal";
 import { useAppDispatch } from "../../app/store/store";
@@ -12,53 +12,70 @@ type GameLinePageProps = {
 };
 const GameLinePage = ({ gameL }: GameLinePageProps): JSX.Element => {
   const [active, setActive] = useState<boolean>(false);
-  const [newAnswer, setNewAnswer] = useState<string>('')
+  const [newAnswer, setNewAnswer] = useState<string>("");
   const [checkAnsw, setCheckAnsw] = useState<boolean>(false);
-  const dispatch =  useAppDispatch()
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
-const onHadleSubmit =  (e: React.FormEvent<HTMLFormElement>): void => {
+  const dispatch = useAppDispatch();
+
+  const onHadleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
     e.preventDefault();
-    setCheckAnsw((prev) => !prev)
-if(newAnswer.trim().toLowerCase() === gameL.Question.answer.trim().toLowerCase()){
-    dispatch(updateGameLineThunk({id: gameL.id, body: gameL.statusQuest }))
-    // setCheckAnsw((prev) => !prev)
-    // setActive((prev)=>!prev)
-    
-  }
-}
-
-
-
-
-
-
+    setIsOpen((prev) => !prev);
+    setCheckAnsw((prev) => !prev);
+    dispatch(updateGameLineThunk({ id: gameL.id, body: gameL.statusQuest }));
+    if (
+      newAnswer.trim().toLowerCase() ===
+      gameL.Question.answer.trim().toLowerCase()
+    ) {
+      dispatch(updateGameLineThunk({ id: gameL.id, body: gameL.statusQuest }));
+      // setCheckAnsw((prev) => !prev)
+      // setActive((prev)=>!prev)
+    }
+  };
 
   return (
     <div className=" GameLinePage">
       {/* кнопка */}
-    <button className="button_GameLine" onClick={()=>setActive((prev)=> !prev)}>
-    {gameL.Question.score}
-    </button>
-      
-
+      {gameL.statusQuest === false ? (
+        <button
+          className="button_GameLine"
+          onClick={() => setActive((prev) => !prev)}
+        >
+          {gameL.Question.score}
+        </button>
+      ) : (
+        <button className="button_GameLine">{gameL.Question.score}</button>
+      )}
 
       {/* модалка */}
-      <ModalWindow  active={active} setActive={setActive}>
-     <h3>{gameL.Question.name}</h3>
-     <form onSubmit={onHadleSubmit}>
-      <input type= 'text' placeholder="Ваш ответ" value={newAnswer} onChange={(e)=>setNewAnswer(e.target.value)}/>
-      <button type="submit">Ответить</button>
-    </form>
+      <ModalWindow active={active} setActive={setActive}>
+        <h3>{gameL.Question.name}</h3>
+        <form onSubmit={onHadleSubmit}>
+          <input
+            type="text"
+            placeholder="Ваш ответ"
+            value={newAnswer}
+            onChange={(e) => setNewAnswer(e.target.value)}
+          />
+          {isOpen === false ? (
+            <button type="submit">Ответить</button>
+          ) : (
+            <button type="button" onClick={() => setActive((prev) => !prev)}>
+              Выйти
+            </button>
+          )}
+        </form>
 
-    {checkAnsw && (
-    <>
-    {newAnswer === gameL.Question.answer ? (
-      <h5>Правильно!</h5>
-    ):(
-      <h5>Увы, правильный ответ {gameL.Question.answer}</h5>
-    )}
-    </>
-    )}
+        {checkAnsw && (
+          <>
+            {newAnswer.trim().toLowerCase() ===
+            gameL.Question.answer.trim().toLowerCase() ? (
+              <h5>Правильно!</h5>
+            ) : (
+              <h5>Увы, правильный ответ {gameL.Question.answer}</h5>
+            )}
+          </>
+        )}
       </ModalWindow>
     </div>
   );
--- server/middleware/verifyAccessToken.js
+++ server/middleware/verifyAccessToken.js
@@ -1,28 +1,28 @@
-const { User, Game } = require('../db/models')
-const jwt = require('jsonwebtoken')
+const { User, Game } = require("../db/models");
+const jwt = require("jsonwebtoken");
 
 async function verifyAccessToken(req, res, next) {
   try {
     // проверяем заголовок на наличие токена
-    const accessToken = req.headers.authorization.split(' ')[1];
-    const { user } = jwt.verify(accessToken, 'A');
-
+    const accessToken = req.headers.authorization.split(" ")[1];
+    let { user } = jwt.verify(accessToken, "A");
+    console.log(user, 1111);
 
     user = await User.findOne({
-        where: { id: user.id },
-        attributes: ['id', 'name', 'email'],
-      });
-  
-    const game = await Game.findOne({where: {userId: user.id}})
+      where: { id: user.id },
+      attributes: ["id", "name", "email"],
+    });
 
-    user.gameId = game.id
-    
+    const game = await Game.findOne({ where: { userId: user.id } });
+    console.log(game, 222);
+    user.gameId = game.id;
+
     res.locals.user = user;
-
+    console.log(user);
     next();
   } catch (error) {
-    console.log('Invalid access token');
-    res.status(403).send('Invalid access token');
+    console.log("Invalid access token");
+    res.status(403).send("Invalid access token");
   }
 }
 
